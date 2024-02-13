8a79c873c5c137460f189dc2f050495303576b70
donghwan12 <155033223+donghwan12@users.noreply.github.com>
2024-02-13 12:47:34 +0900

    2024-02-13

---
+++ javaScript/01.JS_basic/js/05.js
@@ -0,0 +1,6 @@
+console.log("05.js start...");
+const boxE1 = document.querySelector('.box');
+boxE1.addEventListener('click',function(){
+    console.log('clicked....');
+    boxE1.style.backgroundColor='orange';   
+    });
