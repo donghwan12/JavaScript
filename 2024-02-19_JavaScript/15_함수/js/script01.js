e5fe5ea51263a571e02a9909c2352d48f4d57e2f
donghwan12 <155033223+donghwan12@users.noreply.github.com>
2024-02-19 13:15:08 +0900

    Add files via upload

---
+++ 2024-02-19_JavaScript/15_함수/js/script01.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script01.f1 function call')
+};
+const f1_01=()=>{
+    console.log('script01.f1 function call')
+}
---
+++ 2024-02-19_JavaScript/15_함수/js/script02.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script02.f1 function call')
+}
+const f1_01=()=>{
+    console.log('script02.f1 function call')
+}
